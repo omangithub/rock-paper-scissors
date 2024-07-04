15a04e2c70cea917eef1c60adfab43a7cbf507c0
oman <smithowain@hotmail.com>
2024-07-04 10:07:12 +0900

    Working Rock, Paper, Scissors game with comments

--- javascript.js
+++ javascript.js
@@ -5,13 +5,15 @@ console.log("Hello World")
 //define the variables
 //the n variable will be an integer, so I must use parseInt. It must be rounded down, so I use math.floor. And it must be a random number between 1-3, so I will use math.random, times by 3 and +1
 
-n = parseInt(Math.floor(Math.random()*3)+1);
+n = 0
+
 
 let computerChoice = undefined;
 
 // converts the computers random number to a choice
 
   function getComputerChoice() {
+    n = parseInt(Math.floor(Math.random()*3)+1);
       if (n===1) {
         computerChoice = "rock";
       }else if (n===2) {
@@ -21,39 +23,93 @@ let computerChoice = undefined;
       }
     }
 
-getComputerChoice()
-
-  console.log(computerChoice)
-
 //Now I need the users input. Make a function named getHumanChoice. Use prompt to get user's input
 
+let humanChoice=undefined
 
 function getHumanChoice(rock) {
-    var humanChoice = prompt("Choose rock, paper, or scissors :");
-
-        if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You have chosen rock. The computer chose paper. You lose!");
-        return;
-    }else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You have chosen scissors. The computer chose rock. You lose!");
-        return;
-    }else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You have chosen paper. The computer chose scissors. You lose!");
-        return;
-    }else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You have chosen rock. The computer chose scissors. You win!");
-        return;
-    }else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You have chosen scissors. The computer chose paper. You win!");
-        return;
-    }else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You have chosen paper. The computer chose rock. You win!");
-        return;
-    }else{
-        humanChoice=false
-    }
+  humanChoice = prompt("Choose rock, paper, or scissors :");
+  humanChoice = humanChoice.toLowerCase();
 }
 
-getHumanChoice()
+//Now I need to define scores and increment them for wins and losses
 
-console.log(humanChoice)
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound() {
+  getComputerChoice();
+  getHumanChoice();
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You have chosen rock. The computer chose paper. You lose!");
+    computerScore ++;
+    return ;
+  }else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("You have chosen scissors. The computer chose rock. You lose!");
+    computerScore ++;
+    return ;
+  }else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("You have chosen paper. The computer chose scissors. You lose!");
+    computerScore ++;
+    return ;
+  }else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You have chosen rock. The computer chose scissors. You win!");
+    humanScore ++;
+    return ;
+  }else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You have chosen scissors. The computer chose paper. You win!");
+    humanScore ++;
+    return;
+  }else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You have chosen paper. The computer chose rock. You win!");
+    humanScore ++;
+    return;
+  }else if (humanChoice == "rock" && computerChoice == "rock") {
+    console.log("You have chosen rock. The computer chose rock. It's a draw!");
+    return;
+  }else if (humanChoice == "scissors" && computerChoice == "scissors") {
+    console.log("You have chosen scissors. The computer chose scissors. It's a draw!");
+    return;
+  }else if (humanChoice == "paper" && computerChoice == "paper") {
+    console.log("You have chosen paper. The computer chose paper. It's a draw!");
+    return;
+  }else{
+    console.log("Do you even know what you are writing? Try again!")
+    humanChoice=false
+  }
+}
+
+//a function for 5 rounds of play
+
+function playGame() {
+  playRound();
+  console.log(humanScore);
+  console.log(computerScore);
+  playRound();
+  console.log(humanScore);
+  console.log(computerScore);
+  playRound();
+  console.log(humanScore);
+  console.log(computerScore);
+  playRound();
+  console.log(humanScore);
+  console.log(computerScore);
+  playRound();
+  console.log(humanScore);
+  console.log(computerScore);
+  finalScore();
+}
+
+function finalScore() {
+  if (humanScore > computerScore) {
+    console.log("You are the champion!!")
+  } else if (computerScore> humanScore) {
+    console.log("Nice try. The Computer reigns supreme!!")
+  } else {
+    console.log("It's a draw. All's fair that ends square!")
+  }
+}
+
+//run the function to play the game
+
+playGame();
